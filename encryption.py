2f707427eb2da95026a488d78743ade942e30589
galiks <turchenkov.pasha@list.ru>
2018-09-27 00:53:25 +0400

    Steganography and encryption

--- encryption.py
+++ encryption.py
@@ -40,5 +40,50 @@ class Encryption:
         self.decr(steps, self.encr(result, line))
 
 
+class Steganography:
+
+    def __init__(self):
+        # инициализация переменных
+        line = "usual text"
+        lineLength = len(line)
+        startFile = open("startFile.txt", 'r')
+        endFile = open("endFile.txt", 'w+')
+        letter = startFile.read()
+        array = self.text_to_bits(letter)
+        specSymbolForZero = self.text_from_bits(str(100000))
+        specSymbolForOne = self.text_from_bits(str(1111111))
+        line = self.encryption(array, endFile, line, specSymbolForOne, specSymbolForZero)
+        # декодирование
+        self.decryption(line, lineLength, specSymbolForOne, specSymbolForZero)
+
+    def decryption(self, line, lineLength, specSymbolForOne, specSymbolForZero):
+        result = ""
+        for i in range(lineLength, len(line)):
+            if line[i] == specSymbolForZero:
+                result += "0"
+            elif line[i] == specSymbolForOne:
+                result += "1"
+        print(self.text_from_bits(result))
+
+    def encryption(self, array, endFile, line, specSymbolForOne, specSymbolForZero):
+        for item in array:
+            for i in item:
+                if (int(i) & 1) == 1:
+                    line += specSymbolForOne
+                else:
+                    line += specSymbolForZero
+        endFile.write(line)
+        return line
+
+    def text_to_bits(self, text, encoding='utf-8', errors='surrogatepass'):
+        bits = bin(int.from_bytes(text.encode(encoding, errors), 'big'))[2:]
+        return bits.zfill(8 * ((len(bits) + 7) // 8))
+
+    def text_from_bits(self, bits, encoding='utf-8', errors='surrogatepass'):
+        n = int(bits, 2)
+        return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0'
+
+
 if __name__ == '__main__':
-    start = Encryption()
+    start1 = Encryption()
+    start2 = Steganography()
