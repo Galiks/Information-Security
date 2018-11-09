58cd74a1144dce81a8291a5090b53b32a8db73cf
galiks <turchenkov.pasha@list.ru>
2018-11-09 11:55:21 +0400

    Signature

---
+++ Signature/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Signature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool end = false;
+
+            Dictionary<string, string> signature = new Dictionary<string, string>()
+            {
+                { @"word/document", "DOCX" },
+                { @"Excel.Sheet" , "XLS"},
+                { @"workbook.xml", "XLSX"},
+                { "137 80 78 71 13 10 26 10", "PNG"},
+                { "MSWordDoc", "DOC"},
+            };
+
+            List<string> files = new List<string>()
+            {
+                @"E:\Загрузки\CхемаБазы.jpg",
+                @"E:\Загрузки\schedule_do_441.xls",
+                @"E:\Загрузки\Задание.docx",
+                @"E:\Загрузки\qwerty.xlsx",
+                @"E:\Загрузки\Текст для КНиИт.doc",
+            };
+
+
+            //foreach (var filePath in files)
+            //{
+            //    if (end == true)
+            //    {
+            //        break;
+            //    }
+            //    using (StreamReader streamReader = new StreamReader(filePath))
+            //    {
+            //        string line = streamReader.ReadToEnd();
+
+            //        //Console.WriteLine(line);
+
+            //        foreach (var item in signature)
+            //        {
+            //            if (line.Contains(item.Key))
+            //            {
+            //                Console.WriteLine(item.Value);
+            //                end = true;
+            //            }
+            //        }
+            //    }
+
+                //137 80 78 71 13 10 26 10
+
+                byte[] result = null;
+                using (FileStream fileStream = File.OpenRead(@"E:\Загрузки\CхемаБазы.jpg"))
+                {
+                    using (BinaryReader binaryReader = new BinaryReader(fileStream))
+                    {
+                        result = binaryReader.ReadBytes(16);
+
+                        foreach (var item in signature)
+                        {
+                            if (item.ToString().Contains(item.Key))
+                            {
+                                Console.WriteLine(item.Value);
+                                end = true;
+                            }
+                        }
+                    }
+                }
+            //}
+
+            Console.Read();
+        }
+    }
+}
--- Signature/Properties/AssemblyInfo.cs
+++ Signature/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("ConsoleApp2")]
+[assembly: AssemblyTitle("Signature")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp2")]
+[assembly: AssemblyProduct("Signature")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("d8312a9d-4bbf-44df-94a9-4affa234b6d9")]
+[assembly: Guid("0f21543f-6f1c-4f75-8b3a-818f33ea7232")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
